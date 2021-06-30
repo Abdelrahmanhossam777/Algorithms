4e030cdf8c62b9e55f2d552ac8849e9c4dbe05f3
Abdelrahman Hossam <61624734+Abdelrahmanhossam777@users.noreply.github.com>
2021-06-30 07:51:30 +0200

    Update Interpolation_Search.c

--- Interpolation_Search.c
+++ Interpolation_Search.c
@@ -7,24 +7,24 @@
 #include <stdio.h>
 
 /*****************************************************************************************/
-/*Function: interpolation_Search       		                         			         */
-/*I/P Parameters:  int length, int number           			                         */
-/*Returns:it returns index of the number if found or -1 if it is not found               */
-/*Desc:This Function applies the Interpolation search Algorithm                          */
+/*Function: interpolation_Search       		                         			         
+/*I/P Parameters:  int length, int number           			                         
+/*Returns:it returns index of the number if found or -1 if it is not found               
+/*Desc:This Function applies the Interpolation search Algorithm                          
 /*****************************************************************************************/
 int interpolation_Search( int length, int number);
 /******************************************************************************/
-/*Function: simple_sort         		                         			  */
-/*I/P Parameters: int array[], int length               		              */
-/*Returns:it returns a sorted array                                           */
-/*Desc:This Function applies the selection Sort  Algorithm  		          */
+/*Function: simple_sort         		                         			  
+/*I/P Parameters: int array[], int length               		              
+/*Returns:it returns a sorted array                                           
+/*Desc:This Function applies the selection Sort  Algorithm  		          
 /******************************************************************************/
 void simple_sort (int length);
 /******************************************************************************/
-/*Function: swap                		                         			  */
-/*I/P Parameters: int *first, int *second           			              */
-/*Returns:it returns index of the number if found or -1 if it is not found    */
-/*Desc:This Function applies the Linear search Algorithm  		              */
+/*Function: swap                		                         			  
+/*I/P Parameters: int *first, int *second           			              
+/*Returns:it returns index of the number if found or -1 if it is not found    
+/*Desc:This Function applies the Linear search Algorithm  		              
 /******************************************************************************/
 void swap(int *first, int *second);
 
